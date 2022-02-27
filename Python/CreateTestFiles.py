a93ea08e9a670fd06bc0c587530cbd63c7e55cf4
Vdovenko Sergey <zeltyr@yamdex.ru>
2022-02-27 22:54:01 +0700

    Доработан скрипт по очистке указанной папки
    Добавлен фильтр по именам файлов

--- Python/CreateTestFiles.py
+++ Python/CreateTestFiles.py
@@ -6,6 +6,7 @@ abspath = os.path.abspath(dirname)
 
 #генерируем тестовые файлы
 abspath = os.path.join(abspath, "test")
+os.mkdir(abspath)
 
 i = 999
 while i < 1010: 
--- Python/WorkWithFiles.py
+++ Python/WorkWithFiles.py
@@ -2,33 +2,49 @@ from datetime import datetime
 import os
 import time
 
-#__file__ - похоже какая-то переменная окружения, которая хранит путь к текущему файлу питона
+# __file__ - похоже какая-то переменная окружения, которая хранит путь к текущему файлу питона
+
 
 def DeleteFilesInFolder(abspath, curent_date):
     max_size = 50
+    ex_files = ["DMT_Set_DocumentDebtEx",
+                "DMT_Set_BalanceDocumentEx", "DMT_User3_Get_TT_TPE", "text1010"]
     count_del = 0
 
     dirfiles = os.listdir(abspath)
 
     for file in dirfiles:
+
         file_path = os.path.join(abspath, file)
-        if os.path.isdir(file_path):continue
+        if os.path.isdir(file_path):
+            continue
+
+        full_name = os.path.basename(file_path)
+        file_name = os.path.splitext(full_name)[0]
+        if file_name in ex_files:
+            os.remove(file_path)
+            count_del = count_del + 1
+            continue
+        
         file_time = os.path.getatime(file_path)
         file_time = datetime.fromtimestamp(file_time)
+
         file_size = os.path.getsize(file_path)
+
         if file_time < curent_date or file_size > max_size:
             os.remove(file_path)
             count_del = count_del + 1
 
     print("deleted files: " + str(count_del))
 
-# очищаем тестовую папку
-dirname = os.path.dirname(__file__)
 
-abspath = os.path.abspath(dirname)
-abspath = os.path.join(abspath, "test") 
+dirname = os.path.dirname(__file__)  # берём полный путь текущего файла
 
-curent_date = datetime(2022, 2, 27, 22, 15, 58)
+abspath = os.path.abspath(dirname)  # вычленяем путь к папке
+abspath = os.path.join(abspath, "test")  # переходим в папку "test"
 
-DeleteFilesInFolder(abspath, curent_date)
+if os.path.exists(abspath):
 
+    # дата раньше которой надо удалить файлы
+    curent_date = datetime(2022, 2, 27, 22, 15, 59)
+    DeleteFilesInFolder(abspath, curent_date)
